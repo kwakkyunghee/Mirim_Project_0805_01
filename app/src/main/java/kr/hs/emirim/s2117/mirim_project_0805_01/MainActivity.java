972b311d00b9a15e0e86b60860493899845faa2b
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-08-05 13:40:12 +0900

    중간 완성

---
+++ app/src/main/java/kr/hs/emirim/s2117/mirim_project_0805_01/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2117.mirim_project_0805_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
